dbe779fed1c85afffa86d661a2dd0b3e4e6ba8d7
ScarboroughCoral <3249977074@qq.com>
2020-12-16 19:14:41 +0800

    feature: add devdep

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+extern crate image;
+
+use image::{ImageFormat}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,4 +19,9 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, image2html!");
+}
+
+#[wasm_bindgen]
+pub fn translate_image(data: Vec<u8>) {
+    image::load_from_memory_with_format(data, ImageFormat.PNG)
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,12 @@
 import * as wasm from "wasm-game-of-life";
 
-wasm.greet();
+const imageInput = document.getElementById("image");
+imageInput.addEventListener('change',(e) => {
+    const input = e.target;
+    const [image] = input.files;
+    console.log(image)
+    // wasm.greet();
+    let fr = new FileReader();
+    fr.readAsArrayBuffer(image)
+})
+
